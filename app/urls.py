284e4fc6bb1b9e892ad96d48f5285971bd674258
BakStudio <example@gmail.com>
2024-10-12 16:20:32 +0600

    The main app is ready, pages index and about

--- app/urls.py
+++ app/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="for html: index"),
+    path("", include("main.urls", namespace="main")),
 ]
--- main/views.py
+++ main/views.py
@@ -1,17 +1,15 @@
-# from django.http import HttpRequest
 from django.shortcuts import render
 
 
-# def index(request):
-#     return HttpRequest("Hey hey! home page ) ")
-
-
 def index(request):
-    context = {
-        "title": "Zhs Home",
-        "content": "Main page of mall - ZhsMall",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": False,
-    }
+    context = {"title": "Home - Главная", "content": "Юхуу🕺 мебельный магазин"}
     return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {
+        "title": "Home - О нас ",
+        "content": "Знаем знаем! мы крутыыые 😎",
+        "text_on_page": "Целый текс о том, какие мы крутые и в конце чуточку про то, почему этот магазин такой классный, и какой хороший товар :D  ",
+    }
+    return render(request, "main/about.html", context)
