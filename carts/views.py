06b7d103739dbeeacc952dab4214921eb9da2510
BakStudio <example@gmail.com>
2024-10-18 17:10:40 +0600

    Add to cart view, refactor included cart templates

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+from carts.models import Cart
+from goods.models import Product
 
 
-def cart_add(request, product_id): ...
+def cart_add(request, product_slug):
+
+    product = Product.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
 
 
-def cart_change(request, product_id): ...
+def cart_change(request, product_slug): ...
 
 
-def cart_remove(request, product_id): ...
+def cart_remove(request, product_slug): ...
